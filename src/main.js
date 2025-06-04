35503ac4caf77e96312896290cafc366894bc774
Izaak Kuipers <izaak.kuipers@gmail.com>
2025-06-04 21:50:29 +0200

    Move the electron check up

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
+if (!navigator.userAgent.toLowerCase().includes("electron")) return;
+
 const shellPid = +env.get("shell_pid");
 const { proc } = await load("process.js");
-
-if (!navigator.userAgent.toLowerCase().includes("electron")) return;
 
 runApp(proc, $METADATA, shellPid);
